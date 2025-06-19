89c07f45517d87e9806599fc3a7bd43468ab82ba
sureshKrishna05 <sureshkrishnar05@gmail.com>
2025-06-19 17:38:02 +0530

    Commit_19062025

--- DesktopApp/Views/DataEntryPage.xaml.cs
+++ DesktopApp/Views/DataEntryPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using DesktopApp.ViewModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace DesktopApp.Views
 {
@@ -12,7 +15,30 @@ namespace DesktopApp.Views
         {
             InitializeComponent();
             DataContext = new DataEntryViewModel();
-            
+           
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAllTextBoxes(this);
+        }
+
+        private void ClearAllTextBoxes(DependencyObject parent)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is TextBox tb)
+                {
+                    tb.Text = string.Empty;
+                }
+                else
+                {
+                    ClearAllTextBoxes(child); // recursive
+                }
+            }
         }
     }
 }
