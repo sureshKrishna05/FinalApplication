923c4ded864c2a81622bf8cdcf2f7f82f60c2711
sureshKrishna05 <sureshkrishnar05@gmail.com>
2025-07-29 18:26:35 +0530

    ProductEntry Refined

---
+++ DesktopApp/Migrations/20250729114407_AddCostAndCategoryToProducts.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesktopApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCostAndCategoryToProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "COST",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCategory",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "COST",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductCategory",
+                table: "Products");
+        }
+    }
+}
